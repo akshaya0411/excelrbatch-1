892d7e0f134854fb4116e5ddd19feaaa12529fb8
akshaya0411 <akshayamadhusudhan04@gmail.com>
2025-01-04 12:18:18 +0530

    Add files via upload

---
+++ core java/Demo5.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class Demo5 {
+    public static void main(String[] args) {
+          String Username = "Ak";
+          String pwd = "Akshaya";
+          while((Username == "Ak") && (pwd =="Akshaya")) {
+            System.out.println("hello Ak Happy new year");
+            break;
+          }
+
+    }
+    
+}
